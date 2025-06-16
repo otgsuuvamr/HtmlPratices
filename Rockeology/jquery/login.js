b70c1df977369865b4ccd481db111a745ed1a275
Rosendoo <99772476+otgsuuvamr@users.noreply.github.com>
2025-06-16 17:52:09 -0300

    Add files via upload

---
+++ Rockeology/jquery/login.js
@@ -0,0 +1,37 @@
+const users = [];
+
+document.getElementById("cadastro").addEventListener("click", () => {
+    document.getElementById("forms2").classList.toggle("showForms")
+});
+
+document.getElementById("forms-cad").addEventListener("submit", (e) => {
+    e.preventDefault(); 
+
+    const newUser = {
+        usuario: document.getElementById("newuser").value,
+        senha: document.getElementById("newpssw").value
+    };
+
+    usuarios.push(newUser);
+    alert("Cadastro Ok!");
+
+    document.getElementById("forms2").classList.toggle("showForms");
+    document.getElementById("newuser").valeu = "";
+    document.getElementById("newpassw").value = "";
+});
+
+document.getElementById("forms-cad1").addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const nUser = document.getElementById("user").value;
+    const nPass = document.getElementById("pass").value;
+
+    const user_able = usuarios.find((u) => u.usuario === nUser && u.senha === nPass);
+
+    if (user_able) {
+        alert("Acesso autorizado!");
+        window.open("usuario.html", "blank");
+    } else {
+        alert("Usuário ou senha inválido!");
+    }
+});
